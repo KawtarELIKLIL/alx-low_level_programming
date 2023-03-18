3d45d650497983cc29198e6f40c31c6e406b18da
KawtarELIKLIL <eliklil.kawtar@gmail.com>
2023-03-17 18:01:00 -0700

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,11 +14,11 @@ int main(void)
 	{
 		for (m = n + 1; m <= 99; m++)
 		{
-			putchar(n / 10 +'0');
-			putchar(n % 10 +'0');
+			putchar(n / 10 + '0');
+			putchar(n % 10 + '0');
 			putchar(' ');
-			putchar(m / 10 +'0');
-			putchar(m % 10 +'0');
+			putchar(m / 10 + '0');
+			putchar(m % 10 + '0');
 			if (n < 98)
 			{
 				putchar(',');
