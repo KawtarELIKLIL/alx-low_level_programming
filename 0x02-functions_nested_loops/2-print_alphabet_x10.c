1e4621e80e8c419c4f25ea5153366c39eb960650
KawtarELIKLIL <eliklil.kawtar@gmail.com>
2023-03-21 11:48:35 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* printlphabet_x10 - prints the alphabet
+*
+* Return: Always 0 (Success)
+*/
+
+void printlphabet_x10(void)
+{
+	int i;
+	int r;
+
+	for (r = 0; r < 10; r++)
+	{
+		for (i = 97; i <= 122; i++)
+		{
+			_putchar(i);
+		}
+	_putchar('\n');
+	}
+}
