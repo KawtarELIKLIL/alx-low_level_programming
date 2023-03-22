297c2740d248fd1dea46f339caf242c4cbcaeea4
KawtarELIKLIL <eliklil.kawtar@gmail.com>
2023-03-21 18:15:37 -0700

    addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  * add - add two numbers
+  * @a: input first number
+  * @b: input second number
+  * Return: returns addition
+ */
+int add(int a, int b);
+{
+	int s;
+
+	s = a + b;
+	return (s);
+}
