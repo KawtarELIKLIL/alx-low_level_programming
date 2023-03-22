8f717575f009a161e92db5a34c881511aab8d964
KawtarELIKLIL <eliklil.kawtar@gmail.com>
2023-03-21 18:46:57 -0700

    print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  *print_to_98 - prints to 98
+  *@n: the startint number
+  *
+  *return: void
+  */
+void print_to_98(int n)
+{
+	int i = 0;
+	if (n > 98)
+	{
+		for (i = n; i > 97; i--)
+		{
+			if ( i > 98)
+			{
+				printf("%d, ", i);
+			}
+			else
+			{
+				printf("%d", i);
+			}
+		}
+	}
+	else if (n < 98)
+	{
+		for(i = n; i < 99; i++)
+		{
+			if (i <98)
+			{
+				printf("%d, ", i);
+			}
+			else
+			{
+				printf("%d", i);
+			}
+		}
+	}
+	else
+	{
+		printf("%D", 98);
+	}
+	printf("\n");
+}
