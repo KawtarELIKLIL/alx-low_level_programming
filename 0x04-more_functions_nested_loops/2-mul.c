724dfea8f98a8787cd05b9a88d29e20217b5ddba
KawtarELIKLIL <eliklil.kawtar@gmail.com>
2023-03-23 08:11:27 -0700

    mul fonction

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  * add - multiply two numbers
+  * @a: input first number
+  * @b: input second number
+  * Return: returns multiplication
+ */
+int mul(int a, int b)
+{
+	int s;
+
+	s = a * b;
+	return (s);
+}
