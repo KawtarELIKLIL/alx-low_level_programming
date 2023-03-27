a2abf6065cc91aca3f6bf2540fb229bd4999e65d
KawtarELIKLIL <eliklil.kawtar@gmail.com>
2023-03-27 15:02:51 -0700

    print in reverse fonction

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+  *print_rev - prints a string in reverse
+  *@s: input string
+  */
+void print_rev(char *s)
+{
+	int len = 0;
+	int j = 0;
+	int i;
+
+	while (s[j] != '\0')
+	{
+		len++;
+		j++;
+	}
+	i = len;
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
