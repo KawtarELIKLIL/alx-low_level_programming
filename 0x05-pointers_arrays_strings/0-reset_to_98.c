fea17fdcd5b798c359f35823f1c0d77407621e10
KawtarELIKLIL <eliklil.kawtar@gmail.com>
2023-03-27 10:45:34 -0700

    reset to 98 fonction

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-  *rest_to_98 - rests the variable value to 98
+  *reset_to_98 - rests the variable value to 98
   *@n: is the adress of the variavle
   */
 void reset_to_98(int *n)
