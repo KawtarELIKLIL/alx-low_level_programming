64155a180ba52f146bc4b32abb438ca60dd1eb56
KawtarELIKLIL <eliklil.kawtar@gmail.com>
2023-03-27 17:10:24 -0700

    print half fonction

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+  *puts_half - prints half a string
+  *@str: input string
+  */
+void puts_half(char *str)
+{
+	int j = 0;
+	int n;
+
+	while (str[j] != '\0')
+	{
+		j++;
+	}
+	if (j % 2 == 0)
+	{
+		for (n = j / 2; n < j; n++)
+			_putchar(str[n]);
+	}
+	else
+	{
+		for (n = (j - 1) / 2; n < j; n++)
+			_putchar(str[n]);
+	}
+	_putchar('\n');
+}
