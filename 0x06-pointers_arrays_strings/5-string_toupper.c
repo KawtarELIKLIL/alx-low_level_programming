4280a76912215f92f7271e0aa605407fa77119c9
KawtarELIKLIL <eliklil.kawtar@gmail.com>
2023-03-29 16:12:10 -0700

    lower to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,9 +8,10 @@ char *string_toupper(char *s)
 {
 	int i = 0;
 
-	while (s[i] != '0')
+	while (s[i] != '\0')
 	{
-		s[i] = s[i] - 32;
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 32;
 		i++;
 	}
 	return (s);
