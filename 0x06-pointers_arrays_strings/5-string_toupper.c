dd121037ca0fbd4dee86bbfb0857292704af28d0
KawtarELIKLIL <eliklil.kawtar@gmail.com>
2023-03-29 16:03:57 -0700

    lower to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*string_toupper - change lowercase to uppercase
+  *@s: input char
+  *Return: change lowercase to uppercase
+  */
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '0')
+	{
+		s[i] = s[i] - 32;
+		i++;
+	}
+	return (s);
+}
