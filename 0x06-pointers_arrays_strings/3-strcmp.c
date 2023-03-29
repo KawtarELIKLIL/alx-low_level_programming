f7f5150ea63eb503f68b2d776bc72a2c3df7a28f
KawtarELIKLIL <eliklil.kawtar@gmail.com>
2023-03-29 13:50:12 -0700

    fonction thatcompares two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*_strcmp - compares two strings
+  *@s1: input char
+  *@s2: input char
+  *Return: comapres two strings
+  */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+	int j;
+
+	j = s1[i] - s2[i];
+	return (j);
+}
