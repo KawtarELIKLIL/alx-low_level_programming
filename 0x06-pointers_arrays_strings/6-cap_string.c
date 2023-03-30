7db6ae07681ca1ca4ed22fecb22f356d83cebf9b
KawtarELIKLIL <eliklil.kawtar@gmail.com>
2023-03-29 17:35:44 -0700

    capitalize every word

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+*cap_string - capitalizes all words of a string
+  *@s: input char
+  *Return: capitalizes all words of a string
+  */
+char *cap_string(char *s)
+{
+	int i = 1;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			if (!(s[i - 1] >= 'a' && s[i - 1] <= 'z')
+			&& !(s[i - 1] >= 'A' && s[i - 1] <= 'Z')
+			&& !(s[i - 1] >= '0' && s[i - 1] <= '9'))
+				s[i] = s[i] - 32;
+		}
+		i++;
+	}
+	return (s);
+}
