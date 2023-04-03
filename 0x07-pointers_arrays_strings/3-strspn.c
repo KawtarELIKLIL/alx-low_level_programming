520e2b5a26aca28b2f0fe14fc8202b130c56f832
KawtarELIKLIL <eliklil.kawtar@gmail.com>
2023-04-03 16:45:13 -0700

    strspn fonction

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,21 +11,21 @@ unsigned int _strspn(char *s, char *accept)
 	int i, j = 0;
 	int k, l;
 
-	while (accept[j] != '\0')
-		j++;
 	while (s[i] != '\0')
 	{
-		if (s[i] == 32)
+		if (s[i] != '\0')
+		{
+			k = 0;
+			while (accept[k] != '\0')
+			{
+				if (s[i] == accept[k])
+					m++;
+				k++;
+			}
+		}
+		else
 			break;
 		i++;
-	}
-	for (k = 0; k < i; k++)
-	{
-		for (l = 0; l < j; l++)
-		{
-			if (s[k] == accept[l])
-				m++;
-		}
 	}
 	return (m);
 }
