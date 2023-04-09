96ae774b089dc8cd2b543ca5a5607c920c27be9b
KawtarELIKLIL <eliklil.kawtar@gmail.com>
2023-04-09 16:02:40 -0700

    print programe name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print name of program
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+		printf("%s\n", argv[0]);
+	}
+	return (0);
+}
