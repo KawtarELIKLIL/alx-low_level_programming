310fc615dc4762dcaffd6061bff746dcd09aa008
KawtarELIKLIL <eliklil.kawtar@gmail.com>
2023-04-10 15:58:16 -0700

    add argv

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+/**
+ * main - adds positive numbers
+ * @argc: number of argument
+ * @argv: name of argument
+ * Return: Always 0 and 1 if not digits
+ */
+int main(int argc, char *argv[])
+{
+	int add = 0;
+	int i;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (isdigit(argv[i]) == '0')
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			add += atoi(argv[i]);
+		}
+	}
+	printf("%d\n", add);
+	return (0);
+}
