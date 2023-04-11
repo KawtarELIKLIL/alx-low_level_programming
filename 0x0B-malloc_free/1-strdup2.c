0542845152fa91e51b8aa3fe1d84d42aa8a3fc98
KawtarELIKLIL <eliklil.kawtar@gmail.com>
2023-04-11 15:15:28 -0700

    copy string to new pointer

--- 0x0B-malloc_free/1-strdup2.c
+++ 0x0B-malloc_free/1-strdup2.c
@@ -1,6 +1,22 @@
 #include "main.h"
 #include <stdlib.h>
 /**
+*size_str - returns size of str
+*@s: input str
+*Return: size of str
+*/
+int size_str(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
+
+/**
 *_strdup - copy the string in a new pointer
 *@str: input string
 *Return: a pointer or NULL
@@ -8,12 +24,12 @@
 char *_strdup(char *str)
 {
 	char *newp;
-	long unsigned int i;
+	int i;
 
-	newp = malloc(sizeof(str));
+	newp = malloc(size_str(str));
 	if (newp == NULL)
 		return (NULL);
-	for (i = 0; i < sizeof(str); i++)
+	for (i = 0; i < size_str(str); i++)
 		newp[i] = str[i];
 	return (newp);
 }
