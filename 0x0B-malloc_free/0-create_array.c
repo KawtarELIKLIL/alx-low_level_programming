110f735586e43da839bc5f8c59986251c6f997f6
KawtarELIKLIL <eliklil.kawtar@gmail.com>
2023-04-11 14:35:26 -0700

    create array of char

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,8 +14,9 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 	array = malloc(size);
-	for (i =0; i < size; i++)
+	if (array == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
 		array[i] = c;
-	
 	return (array);
 }
