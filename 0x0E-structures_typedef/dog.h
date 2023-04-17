32ff90a507c00367b02ee7a5c69b7bcdd3769967
KawtarELIKLIL <eliklil.kawtar@gmail.com>
2023-04-17 09:31:26 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+*struct dog - dogs informations
+*@name: name of dog
+*@age: age of dog
+*@owner: owner of dog
+*/
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+#endif
