b7a8e2ba518758cdac19eaea62d9f5f8bb72890c
KawtarELIKLIL <eliklil.kawtar@gmail.com>
2023-04-17 13:39:31 -0700

    free dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+*free_dog - fonction that frees dog
+*@d: pointer to my dog
+*/
+void free_dog(dog_t *d);
+{
+	if (d != 0)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
