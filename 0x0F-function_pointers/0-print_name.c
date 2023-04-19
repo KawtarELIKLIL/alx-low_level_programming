6342644f69e1d998092af858109f08b5626b17f2
KawtarELIKLIL <eliklil.kawtar@gmail.com>
2023-04-19 15:18:11 -0700

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+*print_name - prints a name
+*@f: function pointer
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,5 @@
+#ifndef FONCTION_POINTERS_H
+#define FONCTION_POINTERS_H
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+#endif
