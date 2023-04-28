b70d44d8657c51cbecd3295719aa87dab5b74c11
KawtarELIKLIL <eliklil.kawtar@gmail.com>
2023-04-28 09:04:26 -0700

    free

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <string.h>
+#include <stdlib.h>
+/**
+free_list -  frees a list
+*@head: pointer
+*/
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+	temp = head->next;
+	free(head->str);
+	free(head);
+	head = temp;
+	}
+}
