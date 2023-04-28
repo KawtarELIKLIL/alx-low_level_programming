1e33565ce5641b726b163a19bd643ca68edbdd97
KawtarELIKLIL <eliklil.kawtar@gmail.com>
2023-04-28 12:17:45 -0700

    first

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,7 +4,7 @@ void _constructor(void) __attribute__ ((constructor));
 *_constructor - print before main
 *
 */
-void _constructor(voi)
+void _constructor(void)
 {
 	printf("You're beat! and yet, you must allow,\n"
 		"I bore my house upon my back!\n");
