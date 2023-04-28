a0051aa107160f3847c76691fea9adee5d8532ef
KawtarELIKLIL <eliklil.kawtar@gmail.com>
2023-04-28 08:20:44 -0700

    list len

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*list_len - number of elements in linked list
+*@h: pointer
+*Return: number of elements in linked list
+*/
+size_t list_len(const list_t *h)
+{
+	size_t cnt = 0;
+
+	whilen(h)
+	{
+		cnt++;
+		h = h->next;
+	}
+	return (cnt);
+}
