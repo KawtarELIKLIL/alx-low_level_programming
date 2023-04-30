d333f8a177658ddaa929dde01d0d82033577af6b
KawtarELIKLIL <eliklil.kawtar@gmail.com>
2023-04-30 07:30:17 -0700

    nth node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,12 +12,10 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int cont = 0;
 	listint_t *new = head;
 
-	while (new && cont < (index + 1))
+	while (new && cont < index)
 	{
 		new = new->next;
 		cont++;
-		if (cont == index)
-			return (new);
 	}
-	return (NULL);
+	return (new ? new : NULL);
 }
