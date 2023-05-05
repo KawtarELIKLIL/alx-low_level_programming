493f449982ed3b0979e76d9e8209638bf6d42ac1
KawtarELIKLIL <eliklil.kawtar@gmail.com>
2023-05-04 17:56:24 -0700

    clear bit

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -6,7 +6,7 @@
 *@index: given index
 *Return: 1 or -1
 */
-nt clear_bit(unsigned long int *n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= sizeof(n) * 8)
 		return (-1);
