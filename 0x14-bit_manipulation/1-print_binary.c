c4aa62f9f1c51a1b340f604ac9c5a39753b5faa5
KawtarELIKLIL <eliklil.kawtar@gmail.com>
2023-05-04 17:22:18 -0700

    print bi

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*print_binary - prints the binary of a num
+*@n: num
+*/
+void print_binary(unsigned long int n)
+{
+	int bit = sizeof(n) * 8;
+	int i = 0;
+
+	while (bit)
+	{
+		if (n & 1L << --bit)
+		{
+			_putchar('1');
+			i++;
+		}
+		else if
+			_putchar('0');
+	}
+	if (!i)
+		_putchar('0');
+}
